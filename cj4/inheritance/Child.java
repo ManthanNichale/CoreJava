cd763503245b12fb29c1fd7418ca0d49c026af14
ManthanNichale <manthanashru.n@techouts.co.in>
2026-02-20 18:37:42 +0530

    Corejava

--- cj4/inheritance/Child.java
+++
@@ -1,7 +0,0 @@
-package cj4.inheritance;
-
-public class Child extends Parent{
-    void m2(){
-        System.out.println("Child class");
-    }
-}
--- cj4/inheritance/TestInheritance.java
+++
@@ -1,9 +0,0 @@
-package cj4.inheritance;
-
-public class TestInheritance {
-    public static void  main(String[]args){
-        Child ref = new Child(); // is showing only the test class is accesess the both child and parents class
-        ref.m2();
-        ref.m1();
-    }
-}
