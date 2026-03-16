94976c36dcaf0eb639e05b2b8a713bde1c3693a1
ManthanNichale <manthanashru.n@techouts.co.in>
2026-03-16 19:13:57 +0530

    Program

--- jdbc/DemoConnection.java
+++ jdbc/DemoConnection.java
@@ -1,18 +1,21 @@
 package com.techouts.jdbc;
 
 import java.sql.Connection;
-
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DemoConnection {
 
-    static void main() throws ClassNotFoundException, SQLException {
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("org.postgresql.Driver"); // load PostgreSQL driver
+        return DriverManager.getConnection("jdbc:postgresql://localhost:5432/techouts", "postgres", "manthan"
+        );
+    }
 
-Class.forName("org.postgresql.Driver");
-        Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/techouts","postgres","manthan");
-        System.out.println("connected ");
-
-
+    // Optional main method to test the connection
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Connection conn = getConnection();
+        System.out.println("Connected to PostgreSQL successfully!");
+        conn.close();
     }
 }
--- jdbc/DemoInsert.java
+++ jdbc/DemoInsert.java
@@ -7,32 +7,26 @@ import java.sql.Statement;
 
 public class DemoInsert {
 
-    static void main() throws SQLException {
-        Connection conn=ConnectDB.getConnection();
-      //  String sr="INSERT INTO student VALUES(55,'rajiv',99.7)";
-        Statement statement=conn.createStatement();
-      //  int p= statement.executeUpdate(sr);
-       // System.out.println("Record is insert " +" "+p);
+    static void main() throws SQLException ,ClassNotFoundException{
+        Connection conn = DemoConnection.getConnection();
+        Statement statement = conn.createStatement();
+        try {
 
-        //Fetch Data from Database.......
+            String qure = "SELECT name FROM student where roll=141";
+            ResultSet re = statement.executeQuery(qure);
 
-        String qure="SELECT name FROM student;";
-        ResultSet re= statement.executeQuery(qure);
-        while(re.next()){
-            System.out.println(re.getString("name"));
-        }
-        String e="select roll from student where roll<10";
-        ResultSet e1 =statement.executeQuery(e);
-try{
-        while(e1.next()) {
-          //  System.out.println(e1.getInt(e1.getInt(1)+""+e1.getString(2)+""+ e1.getInt(3)));
-        }
+            if(!re.next()){
+                System.out.println("No name found");
+            }
+            else{
+                do{
+                    System.out.println(re.getString("name"));
+                }while(re.next());
+            }
 
         }
-catch (Exception ed){
-    System.out.println("Not present the it any one  from it ");
+        catch(SQLException e) {
+            System.out.println(e);
+        }    }
 }
 
-
-    }
-}
